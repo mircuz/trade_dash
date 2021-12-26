dc7dc32f77b91fa11e07d96a2d837459d904a3f9
Mirco <mirco.meaz@gmail.com>
2021-12-26 20:57:50 +0100

    Added Production environment

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,11 @@
 from src.layout import app
+from waitress import serve
 
+DEBUG_STATUS = False
 
 # MAIN
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    if DEBUG_STATUS == False:
+        serve(app.server, host='0.0.0.0', port=8050)
+    else: 
+        app.run_server(debug=True)
